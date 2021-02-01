04181a14b9b3054fb3bff1f651e10efd7feb2710
art645 <aefanov95@inbox.ru>
2021-02-01 16:53:22 +0300

    1. Delete XPath parser

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,5 @@
-DROP TABLE IF EXISTS currency CASCADE;
 SET CLIENT_ENCODING TO 'utf-8';
-CREATE TABLE currency (
+CREATE TABLE if not exists currency (
                           id VARCHAR(60),
                           numCode VARCHAR(60),
                           charCode VARCHAR(60),
@@ -8,22 +7,7 @@ CREATE TABLE currency (
                           name VARCHAR(60),
                           value VARCHAR(60)
 );
-INSERT INTO currency
-SELECT
-    (xpath('//Valute/@ID', myTempTable.myXmlColumn))[1]::text AS id
-     ,(xpath('//NumCode/text()', myTempTable.myXmlColumn))[1]::text AS numCode
-     ,(xpath('//CharCode/text()', myTempTable.myXmlColumn))[1]::text AS charCode
-     ,(xpath('//Nominal/text()', myTempTable.myXmlColumn))[1]::text::int AS nominal
-     ,(xpath('//Name/text()', myTempTable.myXmlColumn))[1]::text AS name
-     ,(xpath('//Value/text()', myTempTable.myXmlColumn))[1]::text AS value
-FROM unnest(
-             xpath
-                 (    '//ValCurs/Valute'
-                 ,XMLPARSE(DOCUMENT convert_from(pg_read_binary_file('C:\javaProjects\CurrencyConverter\src\main\resources\static\XML_daily.asp'), 'UTF-8'))
-                 )
-         ) AS myTempTable(myXmlColumn)
-;
-INSERT  INTO currency
+INSERT  INTO currency (id, numCode, charCode, nominal, name, value)
     VALUES ('R1',1,'RUS',1,'Российский рубль','1');
 
 create table if not exists Converted_Currency (
