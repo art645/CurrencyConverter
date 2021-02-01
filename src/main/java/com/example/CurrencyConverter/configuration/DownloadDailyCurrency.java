2f73dab469ff272ea84ab41c5c1735acebe19456
art645 <aefanov95@inbox.ru>
2021-02-01 16:54:40 +0300

    1.Added SAXparser 2.Save currency objects in DB

--- src/main/java/com/example/CurrencyConverter/configuration/DownloadDailyCurrency.java
+++ src/main/java/com/example/CurrencyConverter/configuration/DownloadDailyCurrency.java
@@ -1,8 +1,17 @@
 package com.example.CurrencyConverter.configuration;
 
+
+import com.example.CurrencyConverter.dao.CurrencyRepository;
+import com.example.CurrencyConverter.model.Currency;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -10,24 +19,109 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class DownloadDailyCurrency {
+
+    private String filePath = "C:\\javaProjects\\CurrencyConverter\\src\\main\\resources\\static\\XML_daily.asp";
+    private List<Currency> currencies = new ArrayList<>();
+    private CurrencyRepository currencyRepo;
+
+    public DownloadDailyCurrency(CurrencyRepository currencyRepo) {
+        this.currencyRepo = currencyRepo;
+    }
+
     @Bean
-    public void downloadXml() {
+    public void downloadXML() {
         URL url = null;
         try {
             url = new URL("https://www.cbr-xml-daily.ru/daily_utf8.xml");
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        String fileName = "C:\\javaProjects\\CurrencyConverter\\src\\main\\resources\\static\\XML_daily.asp";
-    Path targetPath = new File(fileName).toPath();
+        Path targetPath = new File(filePath).toPath();
         try {
             Files.copy(url.openStream(), targetPath, StandardCopyOption.REPLACE_EXISTING);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    @Bean
+    public void initSAXparser() {
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        SAXParser parser = null;
+        try {
+            parser = factory.newSAXParser();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+        try {
+            XMLpars saxp = new XMLpars();
+            parser.parse(new File("C:\\javaProjects\\CurrencyConverter\\src\\main\\resources\\static\\XML_daily.asp"), saxp);
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Bean
+    public void saveCurrencyInDB() {
+        currencyRepo.saveAll(currencies);
+    }
 
+    private class XMLpars extends DefaultHandler {
+        private Currency currency = new Currency();
+        private String currentElement;
+
+
+        @Override
+        public void endDocument() throws SAXException {
+            for (Currency currency : currencies) {
+                System.out.println(currency.toString());
+            }
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            currentElement = qName;
+            if (currentElement.equalsIgnoreCase("valute")) {
+                String id = attributes.getValue("ID");
+                currency.setId(id);
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            currentElement = "";
+            if (qName.equalsIgnoreCase("valute")) {
+                System.out.println(currency.toString());
+                currencies.add(currency);
+                currency = new Currency();
+            }
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            // получить ид из атрибутов
+            if (currentElement.equals("NumCode")) {
+                currency.setNumcode(Integer.valueOf(new String(ch, start, length)));
+            }
+            if (currentElement.equals("CharCode")) {
+                currency.setCharcode(new String(ch, start, length));
+            }
+            if (currentElement.equals("Nominal")) {
+                currency.setNominal(Integer.valueOf(new String(ch, start, length)));
+            }
+            if (currentElement.equals("Name")) {
+                currency.setName(new String(ch, start, length));
+            }
+            if (currentElement.equals("Value")) {
+                currency.setValue(new String(ch, start, length));
+            }
+        }
     }
 }
